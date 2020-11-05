e54e61ac890156b6ecd0ab4a5129b4e98aa87ee6
Eugene Sokolyuk <evgnsklk@gmail.com>
2020-11-05 22:16:46 +0300

    make the design responsive

--- lng-chat.ui/src/NavbarItem/NavbarItem.js
+++ lng-chat.ui/src/NavbarItem/NavbarItem.js
@@ -7,11 +7,13 @@ export default function NavbarItem({ id, name, iconName, path }) {
   return (
     <div id={id} className="navbar-item">
       <NavLink activeClassName="navbar-item-link-active" to={path}>
-        <Button
-          className="navbar-item-button"
-          startIcon={iconName ? <Icon>{iconName}</Icon> : null}
-        >
-          {name}
+        <Button className="navbar-item-button">
+          <div className="navbar-item-button-wrapper">
+            <span className="navbar-item-button-icon">
+              {iconName ? <Icon>{iconName}</Icon> : null}
+            </span>
+            <span className="navbar-item-button-label">{name}</span>
+          </div>
         </Button>
       </NavLink>
     </div>
