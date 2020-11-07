fe4c16a2f663f2d0d769e3b98d54e0a03c6097d5
Eugene Sokolyuk <evgnsklk@gmail.com>
2020-11-08 02:09:57 +0300

    add validation for limit and offset

--- LngChat/LngChat.Business/Models/PagingFilterModel.cs
+++ LngChat/LngChat.Business/Models/PagingFilterModel.cs
@@ -1,9 +1,13 @@
-﻿namespace LngChat.Business.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LngChat.Business.Models
 {
     public class PagingFilterModel
     {
+        [Range(1,100)]
         public int Limit { get; set; }
 
+        [Range(0, 1000000)]
         public int Offset { get; set; }
     }
 }
