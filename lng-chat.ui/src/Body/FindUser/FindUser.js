02cdb2d3c7e6e4a6c71955787a9e459e2ae90416
Eugene Sokolyuk <evgnsklk@gmail.com>
2020-11-19 11:02:39 +0300

    fetch user list

--- lng-chat.ui/src/Body/FindUser/FindUser.js
+++ lng-chat.ui/src/Body/FindUser/FindUser.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { userService } from "../../Shared/Services/userService";
 
 export default function FindUser() {
-    return (
-        <div>
-            Hello from FindUser!
-        </div>
-    )
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+      const result = await userService
+        .getUserAsync()
+        .then((user) => {
+          return {
+            toTeach: user.languagesToLearn.map((lang) => {
+              return { code: lang.code, levelMax: 4 };
+            }),
+            toLearn: user.languagesToTeach.map((lang) => {
+              return { code: lang.code, levelMin: 1 };
+            }),
+          };
+        })
+        .then((langFilter) =>
+          userService.getUsersAsync(
+            langFilter.toLearn,
+            langFilter.toTeach,
+            20,
+            0
+          )
+        );
+      setIsLoading(false);
+      console.log(result);
+    };
+    fetchUser();
+  }, []);
+
+  return <div>{isLoading ? "Loading ..." : "Hello from FindUser!"}</div>;
 }
--- lng-chat.ui/src/Shared/Services/userService.js
+++ lng-chat.ui/src/Shared/Services/userService.js
@@ -2,13 +2,34 @@ import { api } from "./apiService";
 
 export const userService = {
   getUserAsync,
-  updateUserAsync
+  updateUserAsync,
+  getUsersAsync,
 };
 
-function getUserAsync(){
-    return api.getAsync("users/current").then(response => response.json());
+function getUsersAsync(langsToLearn, langsToTeach, limit, offset) {
+  var searchParams = new URLSearchParams();
+  if (langsToLearn) {
+    searchParams.append("languagesToLearn", JSON.stringify(langsToLearn));
+  }
+  if (langsToTeach) {
+    searchParams.append("languagesToTeach", JSON.stringify(langsToTeach));
+  }
+  if (limit) {
+    searchParams.append("limit", limit);
+    if (offset) {
+      searchParams.append("offset", offset);
+    }
+  }
+
+  return api
+    .getAsync("users?" + searchParams.toString())
+    .then((response) => response.json());
 }
 
-function updateUserAsync(userData){
-    return api.patchAsync("users/current", userData);
+function getUserAsync() {
+  return api.getAsync("users/current").then((response) => response.json());
+}
+
+function updateUserAsync(userData) {
+  return api.patchAsync("users/current", userData);
 }
