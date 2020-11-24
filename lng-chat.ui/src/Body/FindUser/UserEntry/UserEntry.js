f59f99a1eee8a9f2137696c10f33033b234b3da3
Eugene Sokolyuk <evgnsklk@gmail.com>
2020-11-24 14:41:50 +0300

    add tooltips with language level

--- lng-chat.ui/src/Body/FindUser/UserEntry/UserEntry.js
+++ lng-chat.ui/src/Body/FindUser/UserEntry/UserEntry.js
@@ -1,46 +1,61 @@
 import "./UserEntry.css";
-import { Avatar, Box, Button, CircularProgress } from "@material-ui/core";
+import {
+  Avatar,
+  Box,
+  Button,
+  CircularProgress,
+  Tooltip,
+} from "@material-ui/core";
 import { EmojiPeopleRounded, SwapHorizRounded } from "@material-ui/icons";
 import React from "react";
 import { useI18n } from "../../../Shared/i18nContext";
 import { languageService } from "../../../Shared/Services/languageService";
 
+const languageLevels = languageService.getLanguageLevels();
+
 const languageEntry = (code, level) => (
-  <Box className="language-entry" position="relative" display="inline-flex">
-    <Box
-      top={0}
-      left={0}
-      bottom={0}
-      right={0}
-      position="absolute"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <div
-        className="flag-icon"
-        style={{
-          backgroundImage: `url(${
-            languageService.getLanguageMetadata(code).flag
-          })`,
-        }}
+  <Tooltip
+    arrow
+    title={languageLevels.find((x) => x.index === level).shortName}
+    placement="top"
+    aria-label="language"
+  >
+    <Box className="language-entry" position="relative" display="inline-flex">
+      <Box
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        position="absolute"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <div
+          className="flag-icon"
+          style={{
+            backgroundImage: `url(${
+              languageService.getLanguageMetadata(code).flag
+            })`,
+          }}
+        />
+      </Box>
+      <CircularProgress
+        className="language-entry-circle bottom"
+        variant="static"
+        value={100}
+        size={25}
+        thickness={5}
+      />
+      <CircularProgress
+        className="language-entry-circle top"
+        variant="static"
+        value={level * 20}
+        size={25}
+        thickness={5}
       />
     </Box>
-    <CircularProgress
-      className="language-entry-circle bottom"
-      variant="static"
-      value={100}
-      size={25}
-      thickness={5}
-    />
-    <CircularProgress
-      className="language-entry-circle top"
-      variant="static"
-      value={level * 20}
-      size={25}
-      thickness={5}
-    />
-  </Box>
+  </Tooltip>
 );
 
 export default function UserEntry({
