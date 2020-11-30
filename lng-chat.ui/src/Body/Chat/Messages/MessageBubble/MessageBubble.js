413dd75b96edd86524d2258bf651dad4b49a4916
Eugene Sokolyuk <evgnsklk@gmail.com>
2020-11-30 09:59:15 +0300

    display message sent date

--- lng-chat.ui/src/Body/Chat/Messages/MessageBubble/MessageBubble.js
+++ lng-chat.ui/src/Body/Chat/Messages/MessageBubble/MessageBubble.js
@@ -1,17 +1,32 @@
 import "./MessageBubble.css";
 import React from "react";
 
-export default function MessageBubble({userId, message, nextMessage}) {
-  return <div
-  className={`${
-    message.senderId === userId ? "outgoing" : "incoming"
-  } message${
-    !nextMessage ||
-    nextMessage.senderId !== message.senderId
-      ? " last"
-      : ""
-  }`}
->
-  {message.content}
-</div>;
+export default function MessageBubble({ userId, message, nextMessage }) {
+  const sentAtDate = new Date(message.sentAt);
+
+  const isToday = (function () {
+    let nowDate = new Date();
+    return (
+      sentAtDate.getDate() === nowDate.getDate() &&
+      sentAtDate.getMonth() === nowDate.getMonth() &&
+      sentAtDate.getFullYear() === nowDate.getFullYear()
+    );
+  })();
+
+  return (
+    <div
+      className={`${
+        message.senderId === userId ? "outgoing" : "incoming"
+      } message${
+        !nextMessage || nextMessage.senderId !== message.senderId ? " last" : ""
+      }`}
+    >
+      {message.content}
+      <p className="sent-at">
+        {isToday
+          ? sentAtDate.toLocaleTimeString()
+          : sentAtDate.toLocaleDateString()}
+      </p>
+    </div>
+  );
 }
