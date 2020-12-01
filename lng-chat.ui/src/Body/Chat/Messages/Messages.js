9d463714e6c438501eca07f471e340ec0a123831
Eugene Sokolyuk <evgnsklk@gmail.com>
2020-12-01 23:47:01 +0300

    fix messages appearance

--- lng-chat.ui/src/Body/Chat/Messages/Messages.js
+++ lng-chat.ui/src/Body/Chat/Messages/Messages.js
@@ -1,5 +1,5 @@
 import "./Messages.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { chatService } from "../../../Shared/Services/chatService";
 import { useI18n } from "../../../Shared/i18nContext";
 import { CircularProgress, IconButton } from "@material-ui/core";
@@ -10,7 +10,12 @@ import { ArrowBackIosRounded } from "@material-ui/icons";
 
 const limit = 100;
 
-export default function Messages({ chatId, chatName, onBackButtonClick, ...props }) {
+export default function Messages({
+  chatId,
+  chatName,
+  onBackButtonClick,
+  ...props
+}) {
   const userId = parseInt(accountService.userId);
 
   const [isLoading, setIsLoading] = useState(false);
@@ -31,7 +36,10 @@ export default function Messages({ chatId, chatName, onBackButtonClick, ...props
     chatService
       .getMessagesAsync(chatId, limit, limit * page)
       .then((result) => setMessages((c) => c.concat(result ?? [])))
-      .catch(() => console.log("Error while loading message list."))
+      .then(() => {})
+      .catch((error) =>
+        console.error("Error while loading message list", error)
+      )
       .finally(() => setIsLoading(false));
   }, [page, chatId]);
 
@@ -45,22 +53,36 @@ export default function Messages({ chatId, chatName, onBackButtonClick, ...props
         ) : (
           <div className="messages-container">
             <div className="chat-header">
-              <IconButton onClick={onBackButtonClick} className="btn-back" size="small">
+              <IconButton
+                onClick={onBackButtonClick}
+                className="btn-back"
+                size="small"
+              >
                 <ArrowBackIosRounded fontSize="inherit" />
               </IconButton>
               <p>{chatName}</p>
             </div>
             {messages ? (
               <div className="message-list">
-                {messages.map((message, index) => (
-                  <MessageBubble
-                    userId={userId}
-                    message={message}
-                    next-message={
-                      messages.length < index + 1 ? messages[index + 1] : null
+                <div
+                  ref={(bc) => {
+                    if (bc && page === 0 && messages.length > 0) {
+                      bc.scrollTop = bc.scrollHeight;
                     }
-                  />
-                ))}
+                  }}
+                  className="bubbles-container"
+                >
+                  {messages.map((message, index) => (
+                    <MessageBubble
+                      key={message.id}
+                      userId={userId}
+                      message={message}
+                      nextMessage={
+                        messages.length > index + 1 ? messages[index + 1] : null
+                      }
+                    />
+                  ))}
+                </div>
               </div>
             ) : (
               i18n.hereAreNoMessagesYet
